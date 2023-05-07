d3de28dbe4be7a43e352d91a732e950b25c62c63
Kasualix <670067575@qq.com>
2023-05-07 16:32:21 +0800

    port to 1.18.2

--- src/main/java/com/teampotato/cataclysmbossbar/CataclysmBossBar.java
+++ src/main/java/com/teampotato/cataclysmbossbar/CataclysmBossBar.java
@@ -1,10 +1,42 @@
 package com.teampotato.cataclysmbossbar;
 
-import net.minecraft.util.ResourceLocation;
+import L_Ender.cataclysm.init.ModEntities;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiComponent;
+import net.minecraft.client.gui.components.LerpingBossEvent;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.BossEvent;
 import net.minecraftforge.fml.common.Mod;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.UUID;
 
 @Mod(CataclysmBossBar.ID)
 public class CataclysmBossBar {
     public static final ResourceLocation TEXTURE = new ResourceLocation("cataclysm", "textures/gui/bossbar_frames.png");
     public static final String ID = "cataclysmbossbar";
+
+    public static void display(Map<UUID, LerpingBossEvent> events, PoseStack matrix, Minecraft minecraft) {
+        if (events.isEmpty()) return;
+        int width = minecraft.getWindow().getGuiScaledWidth();
+        int j = 12;
+        for (LerpingBossEvent info : events.values()) {
+            int k = width / 2 - 91;
+            String name = info.getName().toString();
+            if (name.contains(Objects.requireNonNull(ModEntities.NETHERITE_MONSTROSITY.get().getRegistryName()).getPath()) || name.contains(Objects.requireNonNull(ModEntities.ENDER_GUARDIAN.get().getRegistryName()).getPath())) {
+                RenderSystem.setShaderTexture(0, CataclysmBossBar.TEXTURE);
+                RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+                if (info.getColor().equals(BossEvent.BossBarColor.PURPLE)) {
+                    GuiComponent.blit(matrix, k - 7, j - 2, -4.0F, 9, 192, 9, 256, 256);
+                } else if (info.getColor().equals(BossEvent.BossBarColor.RED)) {
+                    GuiComponent.blit(matrix, k - 7, j - 2, -4.0F, 0, 192, 9, 256, 256);
+                }
+            }
+            j += 10 + 9;
+            if (j > minecraft.getWindow().getGuiScaledHeight() / 3) break;
+        }
+    }
 }
--- src/main/java/com/teampotato/cataclysmbossbar/mixin/MixinBossOverlayGui.java
+++ src/main/java/com/teampotato/cataclysmbossbar/mixin/MixinBossOverlayGui.java
@@ -1,13 +1,10 @@
 package com.teampotato.cataclysmbossbar.mixin;
 
-import L_Ender.cataclysm.init.ModEntities;
-import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.PoseStack;
 import com.teampotato.cataclysmbossbar.CataclysmBossBar;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.AbstractGui;
-import net.minecraft.client.gui.ClientBossInfo;
-import net.minecraft.client.gui.overlay.BossOverlayGui;
-import net.minecraft.world.BossInfo;
+import net.minecraft.client.gui.components.BossHealthOverlay;
+import net.minecraft.client.gui.components.LerpingBossEvent;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -16,32 +13,15 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Map;
-import java.util.Objects;
 import java.util.UUID;
 
-@Mixin(value = BossOverlayGui.class, priority = 10)
+@Mixin(value = BossHealthOverlay.class, priority = 10)
 public abstract class MixinBossOverlayGui {
-    @Shadow @Final private Map<UUID, ClientBossInfo> events;
+    @Shadow @Final Map<UUID, LerpingBossEvent> events;
     @Shadow @Final private Minecraft minecraft;
 
     @Inject(method = "render", at = @At("TAIL"))
-    private void displayBossBar(MatrixStack matrix, CallbackInfo ci) {
-        if (this.events.isEmpty()) return;
-        int width = this.minecraft.getWindow().getGuiScaledWidth();
-        int j = 12;
-        for (ClientBossInfo info : this.events.values()) {
-            int k = width / 2 - 91;
-            String name = info.getName().toString();
-            if (name.contains(Objects.requireNonNull(ModEntities.NETHERITE_MONSTROSITY.get().getRegistryName()).getPath()) || name.contains(Objects.requireNonNull(ModEntities.ENDER_GUARDIAN.get().getRegistryName()).getPath())) {
-                this.minecraft.textureManager.bind(CataclysmBossBar.TEXTURE);
-                if (info.getColor().equals(BossInfo.Color.PURPLE)) {
-                    AbstractGui.blit(matrix, k - 7, j - 2, -4.0F, 9, 192, 9, 256, 256);
-                } else if (info.getColor().equals(BossInfo.Color.RED)) {
-                    AbstractGui.blit(matrix, k - 7, j - 2, -4.0F, 0, 192, 9, 256, 256);
-                }
-            }
-            j += 10 + 9;
-            if (j > this.minecraft.getWindow().getGuiScaledHeight() / 3) break;
-        }
+    private void displayCataclysmBossBar(PoseStack matrix, CallbackInfo ci) {
+        CataclysmBossBar.display(events, matrix, minecraft);
     }
 }
